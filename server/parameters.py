f6d84edb2b6308bd6b092071fbf2a1e7b17c7d07
Aasrith Chennapragada <chennapragada.aasrith@gmail.com>
2024-10-06 15:45:28 +0100

    Blockchain Parameterisation and Containerisation
    Containerised the blockchain (ganache)
    
    Parameterised all values needed such as blockchain accounts and key extracts, contract addresses and contract abi's
    
    Added a run-server.sh script to start the code and also the required infra (docker-compose with ganache and rabbitmq).
    
    KEY THING TO NOTE BEFORE RUNNING run-server.sh ENSURE THAT YOU HAVE TRUFFLE INSTALLED

---
+++ server/parameters.py
@@ -0,0 +1,27 @@
+import json 
+import os
+from pathlib import Path
+
+COMPILED_CONTRACT_FILE_NAME = 'ElectricityMeterReading.json'
+COTNRACT_ADDRESS_FILE_NAME = 'deployment_config.json'
+CONTRACT_TRUFFLE_COMPILE_DIR = 'blockchain/build/contracts'
+GANACHE_DATA_DIR_PATH = 'docker-config/ganache-data/'
+GANACHE_ACCOUNTS_FILE = 'ganache-accounts.json'
+BLOCKCHAIN_BASE_DIR = 'blockchain'
+BLOCKCHAIN_URL = 'http://127.0.0.1:8545'
+CONTRACT_COMPILE_FILE_PATH = os.path.join(os.curdir, CONTRACT_TRUFFLE_COMPILE_DIR, COMPILED_CONTRACT_FILE_NAME)
+CONTRACT_ADDRESS_FILE_PATH = os.path.join(os.curdir, BLOCKCHAIN_BASE_DIR, COTNRACT_ADDRESS_FILE_NAME)
+PARENT_DIR = str(Path(__file__).parents[1])
+GANACHE_ACCOUNTS_FILE_PATH = os.path.join(PARENT_DIR, GANACHE_DATA_DIR_PATH, GANACHE_ACCOUNTS_FILE)
+
+def load_json_file(file_path): 
+    try: 
+        with open(file_path) as f: 
+            data = json.load(f)
+            return data 
+    except Exception as e: 
+        raise e 
+
+CONTRACT_ABI = load_json_file(CONTRACT_COMPILE_FILE_PATH)['abi']
+CONTRACT_ADDRESS = load_json_file(CONTRACT_ADDRESS_FILE_PATH)['address']
+ACCOUNTS_DATA = load_json_file(GANACHE_ACCOUNTS_FILE_PATH)
