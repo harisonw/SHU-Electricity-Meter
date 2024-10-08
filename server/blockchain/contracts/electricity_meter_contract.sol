dbd21a0bb94dedc6e77aedcb8acea6871aaaaf08
harisonw <87620631+harisonw@users.noreply.github.com>
2024-10-08 15:49:44 +0100

    feat: Added rabbitmq and client communication

--- server/blockchain/contracts/electricity_meter_contract.sol
+++ server/blockchain/contracts/electricity_meter_contract.sol
@@ -5,21 +5,35 @@ contract ElectricityMeterReading
 
 {
 
-    uint public setData;
-    function set(uint x) public
 
-    {
+    struct MeterReading{
+        string uid; 
+        uint256 mtr_reading;
+        address addr;  
+    }
+    
 
-        setData = x;
+    mapping(address => MeterReading[]) private _meterReadings;
 
+    function getMeterReadings() public view returns (MeterReading[] memory){
+        return _meterReadings[msg.sender];
     }
 
-    function get(
+    function getSpecificMeterReading(string memory uidValue) public view returns (MeterReading memory) {
+        uint length = _meterReadings[msg.sender].length;
 
-    ) public view returns (uint) {
+        for (uint i = 0; i < length; i++) {
+            if (keccak256(abi.encodePacked(_meterReadings[msg.sender][i].uid)) == keccak256(abi.encodePacked(uidValue))) {
+                return _meterReadings[msg.sender][i];
+            }
+        }
+        revert("Meter reading not found");
+    }
 
-        return setData;
-
+    function storeMeterReading(string memory uid, uint mtr_reading) public returns (string memory){
+        MeterReading memory reading = MeterReading({uid: uid, mtr_reading: mtr_reading, addr: msg.sender });
+        _meterReadings[msg.sender].push(reading);
+        return uid; 
     }
 
 }
--- server/server.py
+++ server/server.py
@@ -1,11 +1,12 @@
 from rabbit_mq import * 
 from web3 import Web3
 from parameters import CONTRACT_ABI, CONTRACT_ADDRESS, ACCOUNTS_DATA, BLOCKCHAIN_URL
+from uuid import uuid4
 
 if __name__ == '__main__': 
     w3 = Web3(Web3.HTTPProvider(BLOCKCHAIN_URL))
     example_address = Web3.to_checksum_address(list(ACCOUNTS_DATA['addresses'])[0])
     print(example_address)
     contract_instance = w3.eth.contract(address=CONTRACT_ADDRESS, abi=CONTRACT_ABI)
-    print(contract_instance.functions.set(100).transact({'from': example_address}))
-    print(contract_instance.functions.get().call())
+    print(contract_instance.functions.storeMeterReading(str(uuid4()), 100).transact({'from': example_address}))
+    print(contract_instance.functions.getMeterReadings().call({'from': example_address}))
