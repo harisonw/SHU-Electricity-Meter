b8172ca13b653452ecd795f923596f305bce481e
Jasleen Kaur <jasleen.kaur1@bp.com>
2024-10-21 23:19:05 +0100

    added client blockchain

--- server/parameters.py
+++ server/parameters.py
@@ -4,10 +4,10 @@ from pathlib import Path
 
 COMPILED_CONTRACT_FILE_NAME = 'ElectricityMeterReading.json'
 COTNRACT_ADDRESS_FILE_NAME = 'deployment_config.json'
-CONTRACT_TRUFFLE_COMPILE_DIR = 'blockchain/build/contracts'
+CONTRACT_TRUFFLE_COMPILE_DIR = '../blockchain/build/contracts'
 GANACHE_DATA_DIR_PATH = 'docker-config/ganache-data/'
 GANACHE_ACCOUNTS_FILE = 'ganache-accounts.json'
-BLOCKCHAIN_BASE_DIR = 'blockchain'
+BLOCKCHAIN_BASE_DIR = '../blockchain'
 BLOCKCHAIN_URL = 'http://127.0.0.1:8545'
 CONTRACT_COMPILE_FILE_PATH = os.path.join(os.curdir, CONTRACT_TRUFFLE_COMPILE_DIR, COMPILED_CONTRACT_FILE_NAME)
 CONTRACT_ADDRESS_FILE_PATH = os.path.join(os.curdir, BLOCKCHAIN_BASE_DIR, COTNRACT_ADDRESS_FILE_NAME)
@@ -25,3 +25,7 @@ def load_json_file(file_path):
 CONTRACT_ABI = load_json_file(CONTRACT_COMPILE_FILE_PATH)['abi']
 CONTRACT_ADDRESS = load_json_file(CONTRACT_ADDRESS_FILE_PATH)['address']
 ACCOUNTS_DATA = load_json_file(GANACHE_ACCOUNTS_FILE_PATH)
+
+# Get the first address and private key
+first_address = list(ACCOUNTS_DATA['private_keys'].keys())[0]
+first_private_key = ACCOUNTS_DATA['private_keys'][first_address]
