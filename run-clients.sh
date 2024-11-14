fa81cb7635c730baf8d45bdda619d920e3a02e34
Aasrith Chennapragada <chennapragada.aasrith@gmail.com>
2024-11-14 21:40:47 +0000

    starting multiple blockchain clients
    can now start up to 50 clients if needed.

---
+++ run-clients.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the static path to the Python Tkinter app
+app_path="./client/blockchain_client.py"
+
+# Start 10 instances in parallel, each with a unique instance number
+for i in $(seq 1 12); do
+    # Run each instance in the background with the instance number as an argument
+    python3 "$app_path" "$i" &
+    echo "Started instance $i of the blockchain client  app."
+done
+
+# Wait for all background processes to finish
+wait
+echo "All instances started."
+
